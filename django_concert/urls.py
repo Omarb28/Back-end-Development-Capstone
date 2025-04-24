6dd99870a1d519ed1841b006cc5f68659d165164
Omarb28 <omarb28@gmail.com>
2025-04-24 10:15:07 -0400

    Implement django application

--- django_concert/urls.py
+++ django_concert/urls.py
@@ -17,8 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from concert import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("concert.urls")),
+    path("songs/", views.songs, name="songs"),
+    path("photos/", views.photos, name="photos"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("concert/", views.concerts, name="concerts"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
